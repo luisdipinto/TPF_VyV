748080c07c054e7d3e07b8345305a8679bab05ca
Luis Di Pinto <luisdipinto@gmail.com>
2026-03-19 11:26:27 -0300

    compilacion y documentacion: se configuran banderas Wall Wextra, cppcheck y se documenta el analisis estatico

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+// Pines de la tira LED RGB (ejemplo)
+const int pinLedRojo = 15;
+const int pinLedVerde = 2;
+const int pinLedAzul = 4;
+
+void configurarPines() {
+    pinMode(pinLedRojo, OUTPUT);
+    pinMode(pinLedVerde, OUTPUT);
+    pinMode(pinLedAzul, OUTPUT);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    configurarPines();
+    Serial.println("Iniciando ESP32 RGB Web Server...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+int myFunction(int x, int y) { return x + y; }
